7f098f3b32cd67f5712f563ecc6ab6a13991a2c3
Justin Boileau <45305362+jboileau99@users.noreply.github.com>
2023-02-20 14:36:20 -0700

    switch to postgres (#48)

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -15,6 +15,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 import os
 import django_on_heroku
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -88,11 +89,18 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'OPTIONS': {
+            'service': 'my_service',
+            'passfile': '.my_pgpass',
+        },
     }
 }
 
+DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASES = {
+    'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=600)
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
