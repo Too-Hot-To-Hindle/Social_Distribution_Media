1b9d5e2b1576e7dd6f89dbd2bf85d0d35f7bdf7c
syjiao@ualberta.ca <syjiao@ualberta.ca>
2023-03-01 18:35:53 -0700

    fixed auth pathing

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -6,7 +6,7 @@ const BASE_URL = 'http://127.0.0.1:8000'
 
 export const ENDPOINTS = {
     authors: 'authors',
-    authorsAuth: 'authors/authenticate',
+    authorsAuth: 'auth',
     csrf: 'csrf'
 }
 
