0b3a93e40b7776c039137ffde34d33f951dc77d4
Eric Brisson <ebrisson@ualberta.ca>
2023-03-02 23:23:23 -0700

    Friends and true friends now populate on friends page

--- frontend/src/pages/Friends.js
+++ frontend/src/pages/Friends.js
@@ -20,47 +20,77 @@ const Friends = () => {
 
     const [loading, setLoading] = useState(true)
 
-    const [followers, setFollowers] = useState(null);
-    const [following, setFollowing] = useState(null);
+    const [followers, setFollowers] = useState([]);
+    const [following, setFollowing] = useState([]);
+    const [trueFriends, setTrueFriends] = useState([]);
+    const [friends, setFriends] = useState([]);
 
     useEffect(() => {
         setUsername(localStorage.getItem('username'))
         setUserID(localStorage.getItem('author_id'))
     }, [])
 
-    // retrieve all followers
     useEffect(() => {
-        if (userID) {
-            createAPIEndpoint(`authors/${userID}/followers`)
-                .get()
-                .then(res => {
-                    setFollowers(res.data)
-                    setLoading(false)
-                })
-                .catch(err => {
-                    // TODO: Add in error handling
-                    console.log(err)
-                });
+
+        const getFriends = async () => {
+            if (userID) {
+
+                // reset trueFriends and friends - for debugging purposes
+                setTrueFriends([]);
+                setFriends([]);
+
+                const authorDetails = await createAPIEndpoint(`authors/${userID}`).get()
+                const followerIDs = authorDetails.data.followers;
+                const followingIDs = authorDetails.data.following;
+
+                // get all followers
+                var tempFollowers = [];
+                for (const followerID of followerIDs) {
+                    const res = await createAPIEndpoint(`authors/${followerID}`).get()
+                    tempFollowers.push(res.data)
+                }
+
+                // get all following
+                var tempFollowing = [];
+                for (const followingID of followingIDs) {
+                    const res = await createAPIEndpoint(`authors/${followingID}`).get()
+                    tempFollowing.push(res.data)
+                }
+
+                findTrueFriends(tempFollowers, tempFollowing);
+                findRegularFriends(tempFollowers, tempFollowing);
+                setLoading(false)
+            }
         }
+
+        getFriends();
     }, [userID])
 
-    // TODO: integrate
-    // then, to retrieve all following, get user information, and then course through each ID:
-    // useEffect(() => {
-    //     if (userID) {
-    //         createAPIEndpoint(`authors/${userID}`)
-    //             .get()
-    //             .then(res => {
-    //                 console.log(res.data)
-    //                 setFollowing(res.data.following)
-    //                 setLoading(false)
-    //             })
-    //             .catch(err => {
-    //                 // TODO: Add in error handling
-    //                 console.log(err)
-    //             });
-    //     }
-    // }, [userID])
+    const findTrueFriends = (followers, following) => {
+        // for every follower in followers, check if they are also in following
+        // and if so, add them to trueFriends
+        for (let i = 0; i < followers.length; i++) {
+            for (let j = 0; j < following.length; j++) {
+                if (followers[i].id === following[j].id) {
+                    setTrueFriends(prevState => [...prevState, followers[i]])
+                }
+            }
+        }
+
+        console.log(trueFriends)
+    }
+
+    const findRegularFriends = (followers, following) => {
+        // for every follower in followers, check if they are not in following
+        // and if so, add them to friends
+        for (let i = 0; i < followers.length; i++) {
+            for (let j = 0; j < following.length; j++) {
+                if (followers[i].id !== following[j].id) {
+                    setFriends(prevState => [...prevState, followers[i]])
+                }
+            }
+        }
+    }
 
     return (
         <>
@@ -78,7 +108,7 @@ const Friends = () => {
                         </Grid>
 
                         <Grid item xs={12}>
-                            <Divider/>
+                            <Divider />
                         </Grid>
 
                         <Grid item xs={12}>
@@ -86,6 +116,7 @@ const Friends = () => {
                                 No friend requests at this time.
                             </Alert>
                         </Grid>
+
 
                         {/* <Grid item xs={12}>
                             <div style={{ display: "flex", justifyContent: "space-between" }}>
@@ -120,15 +151,15 @@ const Friends = () => {
 
                         {/* if loading is true, and followers is null, show loader */}
                         {/* otherwise, show list of followers */}
-                        {(loading && followers === null) && (
+                        {loading && (
                             <Grid item xs={12}>
                                 <CircularProgress sx={{ marginTop: "50px", marginBottom: "50px" }} />
                             </Grid>
                         )}
 
-                        {(!loading && followers !== null) && (
+                        {!loading && (
                             <>
-                                {followers.map((follower, index) => {
+                                {trueFriends.map((follower, index) => {
                                     return (
                                         <>
                                             <Grid item xs={12}>
@@ -141,10 +172,37 @@ const Friends = () => {
                                                     </div>
 
                                                     <div style={{ display: "flex", alignItems: "center" }}>
-                                                        {/* <Chip label="True Friend" sx={{marginRight: "10px"}}/> */}
-                                                        <IconButton>
+                                                        <Chip label="True Friend" sx={{ marginRight: "10px" }} />
+                                                        {/* <IconButton>
                                                             <CloseIcon />
-                                                        </IconButton>
+                                                        </IconButton> */}
+                                                    </div>
+                                                </div>
+                                            </Grid>
+
+                                            <Grid item xs={12}>
+                                                <Divider />
+                                            </Grid>
+                                        </>
+                                    )
+                                })}
+
+                                {friends.map((follower, index) => {
+                                    return (
+                                        <>
+                                            <Grid item xs={12}>
+                                                <div style={{ display: "flex", justifyContent: "space-between" }}>
+                                                    <div style={{ display: "flex", alignItems: "center" }}>
+                                                        <AccountCircleIcon sx={{ fontSize: "40px", color: "#F5F5F5", marginRight: "10px" }} />
+                                                        <div>
+                                                            <Typography variant="h6" align="left">@{follower.displayName}</Typography>
+                                                        </div>
+                                                    </div>
+
+                                                    <div style={{ display: "flex", alignItems: "center" }}>
+                                                        {/* <IconButton>
+                                                            <CloseIcon />
+                                                        </IconButton> */}
                                                     </div>
                                                 </div>
                                             </Grid>
