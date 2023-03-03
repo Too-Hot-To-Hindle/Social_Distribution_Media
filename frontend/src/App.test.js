abb52771d3add246d4613f1cdd4991ee40cf4a68
Noah Batiuk <batiuk@ualberta.ca>
2023-03-02 22:17:29 -0700

    Unit tests running!

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -6,3 +6,14 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+// https://www.smashingmagazine.com/2020/06/practical-guide-testing-react-applications-jest/
+it("renders without crashing", () => {
+  shallow(<App />);
+});
+
+it("renders Account header", () => {
+  const wrapper = shallow(<App />);
+  const welcome = <h1>Display Active Users Account Details</h1>;
+  expect(wrapper.contains(welcome)).toEqual(true);
+});
