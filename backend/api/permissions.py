90b14a496cb6b279468781ec551e8f662fcf29cc
Justin Boileau <justin@blindnet.io>
2023-03-16 14:25:52 -0600

    remove ip from permission class

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -2,30 +2,26 @@ from rest_framework import permissions
 
 from .models import AllowedNode
 
-LOCAL_IPS = []
-LOCAL_URLS = []
-REMOTE_GET_ALLOWED_VIEWS = ['Authors', 'AuthorDetail', 'Followers', 'FollowersDetail', 'Post', 'PostDetail', 'ImagePosts', 'Comments', 'PostLikes', 'CommentLikes', 'LikedPosts']
-REMOTE_POST_ALLOWED_VIEWS = ['InboxDetail']
+from pprint import pprint
+
+REMOTE_GET_ALLOWED_VIEWS = ['Authors', 'Author Detail', 'Followers', 'Followers Detail', 'Post', 'Post Detail', 'Image Posts', 'Comments', 'Post Likes', 'Comment Likes', 'Liked Posts']
+REMOTE_POST_ALLOWED_VIEWS = ['Inbox Detail']
 
 class LocalAndRemote(permissions.BasePermission):
     """
-    Permission class allowing remote nodes from certain IPs
+    Permission class allowing remote nodes from certain hosts
     """
     
     def has_permission(self, request, view):
-        ip = request.META['REMOTE_ADDR']
-        host = request.META['REMOTE_HOST']
-        print(ip, host, request, view)
-        print(view.get_view_name())
-        print(request.method)
+        host = request.META['HTTP_HOST']
         operationAllowed = request.method == 'GET' and view.get_view_name() in REMOTE_GET_ALLOWED_VIEWS \
             or request.method == 'POST' and view.get_view_name() in REMOTE_POST_ALLOWED_VIEWS
-        nodeAllowed = (AllowedNode.objects.filter(ip=ip) | AllowedNode.objects.filter(host=host)).exists()
+        nodeAllowed = AllowedNode.objects.filter(host=host).exists()
         return operationAllowed and nodeAllowed
 
 class Local(permissions.BasePermission):
     """
-    Permission class allowing remote nodes from certain IPs
+    Permission class allowing remote nodes from certain hosts
     """
     
     def has_permission(self, request, view):
