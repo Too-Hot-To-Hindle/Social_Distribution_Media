068844e99d6018ed0cdd8cc83fb8b3481f649d48
Justin Boileau <justin@blindnet.io>
2023-03-19 00:37:45 -0600

    wip adding support for url ids

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,13 +1,39 @@
-from django.urls import include, path
+from django.urls import include, path, re_path
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from . import views
 from django.views.generic import TemplateView
 
+# Regex pattern to extract uuids into a named argument that is passed to the view function
+uuid_regex = r'[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}'
+author_id_group = r'.*?(?P<author_id>' + uuid_regex + r')'
+foreign_author_id_group = r'.*?(?P<foreign_author_id>' + uuid_regex + r')'
+post_id_group = r'(?P<post_id>' + uuid_regex + r')'
+comment_id_group = r'(?P<comment_id>' + uuid_regex + r')'
+
+import re
+print(r'^authors\/' + author_id_group)
+print(r'^authors\/' + author_id_group + r'\/followers\/' + foreign_author_id_group + r'')
+print(bool(re.match(r'^authors\/' + author_id_group, 'authors/d5a7f5b6-e68c-4e9e-9612-74ddb6664cfc/followers/83ee4e3d-7f1f-4d43-8698-8adb60f2e9bd')))
+
 urlpatterns = [
     path('authors', views.Authors.as_view()),
-    path('authors/<author_id>', views.AuthorDetail.as_view()),
-    path('authors/<author_id>/followers', views.Followers.as_view()),
-    path('authors/<author_id>/followers/<foreign_author_id>', views.FollowersDetail.as_view()),
+
+    # re_path(r'^authors\/' + author_id_group, views.AuthorDetail.as_view()),
+    # re_path(r'^authors\/' + author_id_group + r'\/followers$', views.Followers.as_view()),
+    # re_path(r'^authors\/' + author_id_group + r'\/followers\/' + foreign_author_id_group + r'', views.FollowersDetail.as_view()),
+    # re_path(r'^authors\/' + author_id_group + r'\/posts$', views.Posts.as_view()),
+    # re_path(r'^authors\/' + author_id_group + r'\/posts\/' + post_id_group + r'$', views.PostDetail.as_view()),
+    # re_path(r'^authors\/' + author_id_group + r'\/posts\/' + post_id_group + r'\/image$', views.ImagePosts.as_view()),
+    # re_path(r'^authors\/' + author_id_group + r'\/posts\/' + post_id_group + r'\/comments$', views.Comments.as_view()),
+    # re_path(r'^authors\/' + author_id_group + r'\/posts\/' + post_id_group + r'\/likes$', views.PostLikes.as_view()),
+    # re_path(r'^authors\/' + author_id_group + r'\/posts\/' + post_id_group + r'\/comments\/' + comment_id_group + r'\/likes$', views.CommentLikes.as_view()),
+    # re_path(r'^authors\/' + author_id_group + r'\/liked$', views.LikedPosts.as_view()),
+    # re_path(r'^authors\/' + author_id_group + r'\/inbox$', views.InboxDetail.as_view()),
+    # re_path(r'^authors\/' + author_id_group + r'\/inbox\/followers$', views.FollowRequests.as_view()),
+
+    path('authors/<str:author_id>', views.AuthorDetail.as_view()),
+    path('authors/<str:author_id>/followers', views.Followers.as_view()),
+    path('authors/<str:author_id>/followers/<str:foreign_author_id>', views.FollowersDetail.as_view()),
     path('authors/<author_id>/posts', views.Posts.as_view()),
     path('authors/<author_id>/posts/<post_id>', views.PostDetail.as_view()),
     path('authors/<author_id>/posts/<post_id>/image', views.ImagePosts.as_view()),
