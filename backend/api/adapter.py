7f0ba80d7cc2bc8d937509bdfb9c550aed246b4c
Noah Batiuk <batiuk@ualberta.ca>
2023-03-19 18:04:01 -0600

    group 13 adapter

--- backend/api/adapter.py
+++ backend/api/adapter.py
@@ -9,3 +9,8 @@ class Adapter:
     def extract_uuid(prefix: str, id):
         raise NotImplementedError("Implement extract_uuid() please")
     
+
+class Group13Adapter(Adapter):
+
+    def extract_uuid(prefix: str, id):
+        raise NotImplementedError("Implement extract_uuid() please")
