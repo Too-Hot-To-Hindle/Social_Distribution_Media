6d0bad9b0f36f8c1af5f9297470990d8f391a18a
Noah Batiuk <batiuk@ualberta.ca>
2023-03-19 18:02:26 -0600

    Adapter parent class

---
+++ backend/api/adapter.py
@@ -0,0 +1,11 @@
+
+
+class Adapter:
+
+    def __init(self, prefix, id):
+        self.prefic = prefix
+        self.id = id
+
+    def extract_uuid(prefix: str, id):
+        raise NotImplementedError("Implement extract_uuid() please")
+    
