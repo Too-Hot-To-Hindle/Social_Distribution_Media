d8b8d5ce50e3c15e62be4230e8a9d38a84800356
Noah Batiuk <batiuk@ualberta.ca>
2023-03-21 21:02:26 -0600

    new changes from main

--- backend/api/connections.py
+++ backend/api/connections.py
@@ -33,7 +33,7 @@ class TeamTESTConnection():
             "teamTEST_cache", backend="sqlite", expire_after=300)
         self.session.auth = (self.username, self.password)
 
-    # URL: ://service/authors/
+# URL: ://service/authors/
     def get_authors(self):
         url = self.base_url + "authors"
         response = self.session.get(url)
@@ -343,19 +343,6 @@ class TeamTESTConnection():
                 
                 return comments
 
-    # URL: ://service/authors/{AUTHOR_ID}/inbox/
-    def send_like(self, author_id, body):
-        url = self.base_url + "authors/" + author_id + "/inbox"
-        response = self.session.post(url, body)
-
-        if response.status_code != 200:
-            # TODO: handle error
-            # look in cache?
-            pass
-
-        else:
-            return
-
     # URL: ://service/authors/{AUTHOR_ID}/posts/{POST_ID}/likes
     def get_post_likes(self, author_id, post_id):
         url = self.base_url + "authors/" + author_id + "/posts/" + post_id + "/likes"
@@ -409,26 +396,24 @@ class TeamTESTConnection():
                 pass
 
             else:
-                comments = []
-                for comment in response:
-                    comments.append({
-                        "type": comment.get("type", "N/A"),
-                        "id": comment.get("id", "N/A"),
+                likes = []
+                for like in response:
+                    likes.append({
+                        "type": like.get("type", "N/A"),
+                        "summary": like.get("summary", "N/A"),
                         "author": {
-                            "type": comment.get("author", {}).get("type", "N/A"),
-                            "id": comment.get("author", {}).get("id", "N/A"),
-                            "host": comment.get("author", {}).get("host", "N/A"),
-                            "displayName": comment.get("author", {}).get("displayName", "N/A"),
-                            "url": comment.get("author", {}).get("url", "N/A"),
-                            "github": comment.get("author", {}).get("github", "N/A"),
-                            "profileImage": comment.get("author", {}).get("profileImage", "N/A"),
+                            "type": like.get("author", {}).get("type", "N/A"),
+                            "id": like.get("author", {}).get("id", "N/A"),
+                            "host": like.get("author", {}).get("host", "N/A"),
+                            "displayName": like.get("author", {}).get("displayName", "N/A"),
+                            "url": like.get("author", {}).get("url", "N/A"),
+                            "github": like.get("author", {}).get("github", "N/A"),
+                            "profileImage": like.get("author", {}).get("profileImage", "N/A"),
                         },
-                        "comment": comment.get("comment", "N/A"),
-                        "contentType": comment.get("contentType", "N/A"),
-                        "published": comment.get("published", "N/A"),
+                        "object": like.get("object", "N/A"),
                     })
                 
-                return comments
+                return likes
 
     # URL: ://service/authors/{AUTHOR_ID}/liked
     def get_author_liked(self, author_id):
@@ -466,17 +451,70 @@ class TeamTESTConnection():
                 return likes
 
     # URL: ://service/authors/{AUTHOR_ID}/inbox
-    def send_to_inbox(self, author_id, body):
+    def send_post(self, author_id, body):
         url = self.base_url + "authors/" + author_id + "/inbox"
-        response = self.session.post(url, body)
+        response = self.session.post(url=url, json=body)
 
-        if response.status_code != 200:
+        if response.status_code != 200 and response.status_code != 201:
             # TODO: handle error
             # look in cache?
+            print("error occurred")
             pass
 
         else:
-            return
+            # might wanna return something, parse the response...
+            print("sent remotely!")
+            return response.json()
+        
+    # URL: ://service/authors/{AUTHOR_ID}/inbox/
+    def send_like(self, author_id, body):
+        url = self.base_url + "authors/" + author_id + "/inbox"
+        response = self.session.post(url=url, json=body)
+
+        if response.status_code != 201:
+            # TODO: handle error
+            # look in cache?
+            print("error occurred")
+            pass
+
+        else:
+            # might wanna return something, parse the response...
+            print("sent remotely!")
+            return response.json()
+        
+    # URL: ://service/authors/{AUTHOR_ID}/inbox/
+    def send_comment(self, author_id, body):
+        url = self.base_url + "authors/" + author_id + "/inbox"
+        response = self.session.post(url=url, json=body)
+
+        if response.status_code != 201:
+            # TODO: handle error
+            # look in cache?
+            print("error occurred")
+            pass
+
+        else:
+            # might wanna return something, parse the response...
+            print("sent remotely!")
+            return response.json()
+        
+    # URL: ://service/authors/{AUTHOR_ID}/inbox/
+    def send_follow(self, author_id, body):
+        url = self.base_url + "authors/" + author_id + "/inbox"
+        response = self.session.post(url=url, json=body)
+
+        if response.status_code != 201:
+            # TODO: handle error
+            # look in cache?
+            print("error occurred")
+            pass
+
+        else:
+            # might wanna return something, parse the response...
+            print("sent remotely!")
+            return response.json()
+
+
 
 
 class Team6Connection():
@@ -490,7 +528,6 @@ class Team6Connection():
         self.session.auth = (self.username, self.password)
 
     # URL: ://authors/
-    # TODO
     def get_authors(self):
         url = self.base_url + "authors"
         response = self.session.get(url)
@@ -526,7 +563,6 @@ class Team6Connection():
                 }
 
     # URL: ://service/authors/{AUTHOR_ID}/
-    # TODO
     def get_single_author(self, author_id):
         url = self.base_url + "authors/" + author_id
         response = self.session.get(url)
@@ -554,7 +590,6 @@ class Team6Connection():
                 }
 
     # URL: ://service/authors/{AUTHOR_ID}/followers
-    # TODO
     def get_author_followers(self, author_id):
         url = self.base_url + "authors/" + author_id + "/followers"
         response = self.session.get(url)
@@ -590,7 +625,6 @@ class Team6Connection():
                 }
 
     # URL: ://service/authors/{AUTHOR_ID}/followers/{FOREIGN_AUTHOR_ID}
-    # TODO
     def check_if_follower(self, author_id, follower_id):
         url = self.base_url + "authors/" + author_id + "/followers/" + follower_id
         response = self.session.get(url)
@@ -612,7 +646,6 @@ class Team6Connection():
                 }
 
     # URL: ://service/authors/{AUTHOR_ID}/posts/{POST_ID}
-    # TODO
     def get_single_post(self, author_id, post_id):
         url = self.base_url + "authors/" + author_id + "/posts/" + post_id
         response = self.session.get(url)
@@ -683,7 +716,6 @@ class Team6Connection():
                 }
 
     # URL: ://service/authors/{AUTHOR_ID}/posts/
-    # TODO
     def get_recent_posts(self, author_id):
         url = self.base_url + "authors/" + author_id + "/posts"
         response = self.session.get(url)
@@ -758,13 +790,11 @@ class Team6Connection():
                 return posts
 
     # URL: ://service/authors/{AUTHOR_ID}/posts/{POST_ID}/image
-    # TODO
     def get_image_post(self, author_id, post_id):
         # TODO: implement this method
         pass
 
     # URL: ://service/authors/{AUTHOR_ID}/posts/{POST_ID}/comments
-    # TODO
     def get_comments(self, author_id, post_id):
 
         url = self.base_url + "authors/" + author_id + "/posts/" + post_id + "/comments"
@@ -807,22 +837,7 @@ class Team6Connection():
                 
                 return comments
 
-    # URL: ://service/authors/{AUTHOR_ID}/inbox/
-    # TODO
-    def send_like(self, author_id, body):
-        url = self.base_url + "authors/" + author_id + "/inbox"
-        response = self.session.post(url, body)
-
-        if response.status_code != 200:
-            # TODO: handle error
-            # look in cache?
-            pass
-
-        else:
-            return
-
     # URL: ://service/authors/{AUTHOR_ID}/posts/{POST_ID}/likes
-    # TODO
     def get_post_likes(self, author_id, post_id):
         url = self.base_url + "authors/" + author_id + "/posts/" + post_id + "/likes"
         response = self.session.get(url)
@@ -859,7 +874,6 @@ class Team6Connection():
                 return likes
 
     # URL: ://service/authors/{AUTHOR_ID}/posts/{POST_ID}/comments/{COMMENT_ID}/likes
-    # TODO
     def get_comment_likes(self, author_id, post_id, comment_id):
         url = self.base_url + "authors/" + author_id + "/posts/" + post_id + "/comments/" + comment_id + "/likes"
         response = self.session.get(url)
@@ -876,29 +890,26 @@ class Team6Connection():
                 pass
 
             else:
-                comments = []
-                for comment in response:
-                    comments.append({
-                        "type": comment.get("type", "N/A"),
-                        "id": comment.get("id", "N/A"),
+                likes = []
+                for like in response:
+                    likes.append({
+                        "type": like.get("type", "N/A"),
+                        "summary": like.get("summary", "N/A"),
                         "author": {
-                            "type": comment.get("author", {}).get("type", "N/A"),
-                            "id": comment.get("author", {}).get("id", "N/A"),
-                            "host": comment.get("author", {}).get("host", "N/A"),
-                            "displayName": comment.get("author", {}).get("displayName", "N/A"),
-                            "url": comment.get("author", {}).get("url", "N/A"),
-                            "github": comment.get("author", {}).get("github", "N/A"),
-                            "profileImage": comment.get("author", {}).get("profileImage", "N/A"),
+                            "type": like.get("author", {}).get("type", "N/A"),
+                            "id": like.get("author", {}).get("id", "N/A"),
+                            "host": like.get("author", {}).get("host", "N/A"),
+                            "displayName": like.get("author", {}).get("displayName", "N/A"),
+                            "url": like.get("author", {}).get("url", "N/A"),
+                            "github": like.get("author", {}).get("github", "N/A"),
+                            "profileImage": like.get("author", {}).get("profileImage", "N/A"),
                         },
-                        "comment": comment.get("comment", "N/A"),
-                        "contentType": comment.get("contentType", "N/A"),
-                        "published": comment.get("published", "N/A"),
+                        "object": like.get("object", "N/A"),
                     })
                 
-                return comments
+                return likes
 
     # URL: ://service/authors/{AUTHOR_ID}/liked
-    # TODO
     def get_author_liked(self, author_id):
         url = self.base_url + "authors/" + author_id + "/liked"
         response = self.session.get(url)
@@ -934,15 +945,65 @@ class Team6Connection():
                 return likes
 
     # URL: ://service/authors/{AUTHOR_ID}/inbox
-    # TODO
-    def send_to_inbox(self, author_id, body):
+    def send_post(self, author_id, body):
         url = self.base_url + "authors/" + author_id + "/inbox"
-        response = self.session.post(url, body)
+        response = self.session.post(url=url, json=body)
 
-        if response.status_code != 200:
+        if response.status_code != 200 and response.status_code != 201:
             # TODO: handle error
             # look in cache?
+            print("error occurred")
             pass
 
         else:
-            return
+            # might wanna return something, parse the response...
+            print("sent remotely!")
+            return response.json()
+        
+    # URL: ://service/authors/{AUTHOR_ID}/inbox/
+    def send_like(self, author_id, body):
+        url = self.base_url + "authors/" + author_id + "/inbox"
+        response = self.session.post(url=url, json=body)
+
+        if response.status_code != 201:
+            # TODO: handle error
+            # look in cache?
+            print("error occurred")
+            pass
+
+        else:
+            # might wanna return something, parse the response...
+            print("sent remotely!")
+            return response.json()
+        
+    # URL: ://service/authors/{AUTHOR_ID}/inbox/
+    def send_comment(self, author_id, body):
+        url = self.base_url + "authors/" + author_id + "/inbox"
+        response = self.session.post(url=url, json=body)
+
+        if response.status_code != 201:
+            # TODO: handle error
+            # look in cache?
+            print("error occurred")
+            pass
+
+        else:
+            # might wanna return something, parse the response...
+            print("sent remotely!")
+            return response.json()
+        
+    # URL: ://service/authors/{AUTHOR_ID}/inbox/
+    def send_follow(self, author_id, body):
+        url = self.base_url + "authors/" + author_id + "/inbox"
+        response = self.session.post(url=url, json=body)
+
+        if response.status_code != 201:
+            # TODO: handle error
+            # look in cache?
+            print("error occurred")
+            pass
+
+        else:
+            # might wanna return something, parse the response...
+            print("sent remotely!")
+            return response.json()
