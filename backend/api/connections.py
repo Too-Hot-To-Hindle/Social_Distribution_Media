a6f2b3e7c859695be3d74b64f0b8191c277c6b07
Eric Brisson <ebrisson@ualberta.ca>
2023-03-23 00:24:41 -0600

    Integrated getting remote comment likes

--- backend/api/connections.py
+++ backend/api/connections.py
@@ -387,29 +387,25 @@ class TeamCloneConnection():
     # URL: ://service/authors/{AUTHOR_ID}/posts/{POST_ID}/likes
     def get_post_likes(self, author_id, post_id):
         url = self.base_url + "authors/" + author_id + "/posts/" + post_id + "/likes"
-        
-        # handle pagination
-        # start at page 1, loop until no items are returned - in which case our server throws a 404
-        page = 1
+
         likes = []
-        while True:
-            response = self.session.get(url, params={"page": page, "size": 10})
+        
+        response = self.session.get(url)
 
-            # no need to handle the 404 using an exception, just return the likes we have/or the empty list
-            if response.status_code == 404:
-                break
+        # no need to handle the 404 using an exception, just return the likes we have/or the empty list
+        if response.status_code == 404:
+            likes = []
 
-            elif response.status_code != 200:
-                raise RemoteServerError("Error getting likes for post with id " + post_id + " from author with id " + author_id + " from remote server: https://social-distribution-media-2.herokuapp.com/; status code " + str(response.status_code) + " was received in response.")
-            
-            response_likes = response.json()
-            if response_likes is None:
-                raise RemoteServerError("Error getting likes for post with id " + post_id + " from author with id " + author_id + " from remote server: https://social-distribution-media-2.herokuapp.com/; no JSON was received in response.")
-            
-            items = response_likes.get("items")
-            if len(items) > 0:
-                likes.extend(items)
-                page += 1
+        elif response.status_code != 200:
+            raise RemoteServerError("Error getting likes for post with id " + post_id + " from author with id " + author_id + " from remote server: https://social-distribution-media-2.herokuapp.com/; status code " + str(response.status_code) + " was received in response.")
+        
+        response_likes = response.json()
+        if response_likes is None:
+            raise RemoteServerError("Error getting likes for post with id " + post_id + " from author with id " + author_id + " from remote server: https://social-distribution-media-2.herokuapp.com/; no JSON was received in response.")
+        
+        items = response_likes.get("items")
+        if len(items) > 0:
+            likes.extend(items)
 
         return {
             "type": "likes",
@@ -419,38 +415,30 @@ class TeamCloneConnection():
     # URL: ://service/authors/{AUTHOR_ID}/posts/{POST_ID}/comments/{COMMENT_ID}/likes
     def get_comment_likes(self, author_id, post_id, comment_id):
         url = self.base_url + "authors/" + author_id + "/posts/" + post_id + "/comments/" + comment_id + "/likes"
+        
+        likes = []
+        
         response = self.session.get(url)
 
-        if response.status_code != 200:
-            # TODO: handle error
-            # look in cache?
-            pass
+        # no need to handle the 404 using an exception, just return the likes we have/or the empty list
+        if response.status_code == 404:
+            likes = []
 
-        else:
-            response = response.json()
-            if response is None:
-                # TODO: handle error
-                pass
+        elif response.status_code != 200:
+            raise RemoteServerError("Error getting likes for comment with id " + comment_id + " from post with id " + post_id + " from author with id " + author_id + " from remote server: https://social-distribution-media-2.herokuapp.com/; status code " + str(response.status_code) + " was received in response.")
+        
+        response_likes = response.json()
+        if response_likes is None:
+            raise RemoteServerError("Error getting likes for comment with id " + comment_id + " from post with id " + post_id + " from author with id " + author_id + " from remote server: https://social-distribution-media-2.herokuapp.com/; no JSON was received in response.")
+        
+        items = response_likes.get("items")
+        if len(items) > 0:
+            likes.extend(items)
 
-            else:
-                likes = []
-                for like in response:
-                    likes.append({
-                        "type": like.get("type", "N/A"),
-                        "summary": like.get("summary", "N/A"),
-                        "author": {
-                            "type": like.get("author", {}).get("type", "N/A"),
-                            "id": like.get("author", {}).get("id", "N/A"),
-                            "host": like.get("author", {}).get("host", "N/A"),
-                            "displayName": like.get("author", {}).get("displayName", "N/A"),
-                            "url": like.get("author", {}).get("url", "N/A"),
-                            "github": like.get("author", {}).get("github", "N/A"),
-                            "profileImage": like.get("author", {}).get("profileImage", "N/A"),
-                        },
-                        "object": like.get("object", "N/A"),
-                    })
-                
-                return likes
+        return {
+            "type": "likes",
+            "items": likes
+        }
 
     # URL: ://service/authors/{AUTHOR_ID}/liked
     def get_author_liked(self, author_id):
--- backend/api/views.py
+++ backend/api/views.py
@@ -609,6 +609,7 @@ class PostLikes(APIView):
                 likes = Like.objects.filter(object__endswith=f'authors/{author_id}/posts/{post_id}')
                 serializer = LikesSerializer({'items': likes})
                 return Response(serializer.data, status=status.HTTP_200_OK)
+            
             except Exception as e:
                 traceback.print_exc()
                 return Response(status=status.HTTP_404_NOT_FOUND)
@@ -633,9 +634,13 @@ class CommentLikes(APIView):
             remote_url = get_remote_url(author_id)
             remote = RemoteConnection(remote_url)
             author_id = extract_uuid_if_url('author', author_id)
-            response = remote.connection.get_comment_likes(author_id, post_id, comment_id)
 
-            return JsonResponse(response, safe=False, status=status.HTTP_200_OK)
+            try:
+                response = remote.connection.get_comment_likes(author_id, post_id, comment_id)
+                return JsonResponse(response, safe=False, status=status.HTTP_200_OK)
+            
+            except Exception as e:
+                return Response(e.args, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
         
         else:
             # Extract a uuid if id was given in the form http://somehost/authors/<uuid>
@@ -643,7 +648,7 @@ class CommentLikes(APIView):
             post_id = extract_uuid_if_url('post', post_id)
             comment_id = extract_uuid_if_url('comment', comment_id)
             if not (author_id and post_id and comment_id):
-                return Response(status=status.HTTP_400_BAD_REQUEST)
+                return Response(status=status.HTTP_404_NOT_FOUND)
             
             try:
                 if not (Author.objects.filter(pk=author_id).exists() and Post.objects.filter(pk=post_id).exists() and Comment.objects.filter(pk=comment_id).exists()):
@@ -654,9 +659,10 @@ class CommentLikes(APIView):
                 likes = Like.objects.filter(object__endswith=f'authors/{author_id}/posts/{post_id}/comments/{comment_id}')
                 serializer = LikesSerializer({'items': likes})
                 return Response(serializer.data, status=status.HTTP_200_OK)
+            
             except Exception as e:
                 traceback.print_exc()
-                return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+                return Response(status=status.HTTP_404_NOT_FOUND)
 
 class LikedPosts(APIView):
 
